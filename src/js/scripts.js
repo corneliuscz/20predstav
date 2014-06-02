2f780106b37a6592bb7339d228446873ec7fa48e
Josef Vybíral <josef.vybiral@dobra-rada.cz>
2014-06-02 17:48:08 +0200

    Layout úvodní stránky, sdílecí buttony, OpenGraph

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,4 +6,14 @@
     // FireShell
   });
 
+  /*
+   * Scroll to top + animate it
+   */
+  $('a.nahoru').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
+
 })(jQuery, window, document);
